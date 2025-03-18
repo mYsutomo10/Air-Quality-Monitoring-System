cb1bfe4a3fc9053a74d96ca4ab79bc0d6af5b763
mYsutomo10 <57779080+mYsutomo10@users.noreply.github.com>
2025-03-19 03:21:15 +0700

    update dataflow

---
+++ data-processing/dataflow_job_update.py
@@ -0,0 +1,187 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io.gcp.bigquery import WriteToBigQuery
+import json
+import os
+from datetime import datetime
+from google.cloud import firestore
+from google.cloud.firestore import Query
+from google.cloud import aiplatform
+
+# Parse pesan dari Pub/Sub
+class ParsePubSubMessage(beam.DoFn):
+    def process(self, message):
+        data = json.loads(message.decode('utf-8'))
+        attributes = data.get("message", {}).get("attributes", {})
+
+        sensor_data = {
+            "location": attributes.get("location", ""),
+            "dt": attributes.get("dt", ""),
+            "lat": float(attributes.get("lat", 0)),
+            "lon": float(attributes.get("lon", 0)),
+            "pm2_5": float(attributes.get("pm2_5", 0)),
+            "pm10": float(attributes.get("pm10", 0)),
+            "o3": float(attributes.get("o3", 0)),
+            "co": float(attributes.get("co", 0)),
+            "no2": float(attributes.get("no2", 0))
+        }
+        yield sensor_data
+
+# Data cleaning
+class CleanSensorData(beam.DoFn):
+    def process(self, element):
+        try:
+            element["dt"] = datetime.strptime(element["dt"], "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return  
+
+        if not (0 <= element["pm2_5"] <= 500):
+            element["pm2_5"] = 0
+        if not (0 <= element["pm10"] <= 600):
+            element["pm10"] = 0
+
+        yield element
+
+# Sinkronisasi dengan data cuaca dari Firestore
+class JoinWeatherData(beam.DoFn):
+    def setup(self):
+        self.db = firestore.Client()
+
+    def process(self, element):
+        location_name = element["location"]
+        query = (
+            self.db.collection("weather_pollution")
+            .document(location_name)
+            .collection("readings")
+            .order_by("dt", direction=Query.DESCENDING)
+            .limit(1)
+        )
+        docs = query.stream()
+
+        for doc in docs:
+            weather_data = doc.to_dict()
+            element.update({
+                "temperature": weather_data["main"]["temp"],
+                "humidity": weather_data["main"]["humidity"],
+                "wind_speed": weather_data["wind"]["speed"],
+                "wind_deg": weather_data["wind"]["deg"]
+            })
+        yield element
+
+# Memanggil model kalibrasi dari Vertex AI
+class CallCalibrationModel(beam.DoFn):
+    def setup(self):
+        self.endpoint = aiplatform.Endpoint(
+            endpoint_name="projects/YOUR_PROJECT_ID/locations/us-central1/endpoints/YOUR_CALIBRATION_ENDPOINT_ID"
+        )
+
+    def process(self, element):
+        instance = {
+            "pm2_5": element["pm2_5"],
+            "pm10": element["pm10"],
+            "temperature": element["temperature"],
+            "humidity": element["humidity"],
+            "wind_speed": element["wind_speed"],
+        }
+
+        response = self.endpoint.predict(instances=[instance])
+        predictions = response.predictions[0]
+
+        element["pm2_5_corrected"] = predictions["pm2_5_corrected"]
+        element["pm10_corrected"] = predictions["pm10_corrected"]
+        yield element
+
+# Menyimpan hasil kalibrasi ke Firestore
+class SaveToFirestore(beam.DoFn):
+    def __init__(self, collection="processed_data"):
+        self.collection = collection
+
+    def setup(self):
+        self.db = firestore.Client()
+
+    def process(self, element):
+        element["dt"] = element["dt"].isoformat()
+        _, write_time = (
+            self.db.collection(self.collection)
+            .document(element["location"])
+            .collection("sensor_info")
+            .add(element)
+        )
+        yield element
+
+# Memanggil model prediksi kualitas udara
+class CallPredictionModel(beam.DoFn):
+    def setup(self):
+        self.endpoint = aiplatform.Endpoint(
+            endpoint_name="projects/YOUR_PROJECT_ID/locations/us-central1/endpoints/YOUR_PREDICTION_ENDPOINT_ID"
+        )
+
+    def process(self, element):
+        instance = {
+            "pm2_5_corrected": element["pm2_5_corrected"],
+            "pm10_corrected": element["pm10_corrected"],
+            "temperature": element["temperature"],
+            "humidity": element["humidity"],
+            "wind_speed": element["wind_speed"],
+            "hour": element["dt"].hour,
+            "day": element["dt"].day,
+            "month": element["dt"].month
+        }
+
+        response = self.endpoint.predict(instances=[instance])
+        predictions = response.predictions[0]
+
+        element["pm2_5_predicted"] = predictions["pm2_5_predicted"]
+        element["pm10_predicted"] = predictions["pm10_predicted"]
+        yield element
+
+# Jalankan pipeline Dataflow
+def run():
+    project_id = os.getenv("GCP_PROJECT", "your-gcp-project")
+    pipeline_options = PipelineOptions(
+        streaming=True,
+        project=project_id,
+        region="us-central1",
+        job_name="aqms-dataflow-pipeline",
+        temp_location=f"gs://{os.getenv('BUCKET_NAME', 'your-bucket-name')}/temp",
+    )
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        (
+            p
+            | "Read PubSub Messages" >> beam.io.ReadFromPubSub(subscription=f"projects/{project_id}/subscriptions/sensor-sub")
+            | "Parse Messages" >> beam.ParDo(ParsePubSubMessage())
+            | "Clean Data" >> beam.ParDo(CleanSensorData())
+            | "Join Weather Data" >> beam.ParDo(JoinWeatherData())
+            | "Call Calibration Model" >> beam.ParDo(CallCalibrationModel())
+            | "Save Calibrated Data to Firestore" >> beam.ParDo(SaveToFirestore(collection="calibrated_data"))
+            | "Call Prediction Model" >> beam.ParDo(CallPredictionModel())
+            | "Save Predicted Data to Firestore" >> beam.ParDo(SaveToFirestore(collection="predicted_data"))
+            | "Save to BigQuery" >> WriteToBigQuery(
+                table=f"{project_id}:aqms_data.sensor_readings",
+                schema="""
+                    location:STRING,
+                    timestamp:TIMESTAMP,
+                    latitude:FLOAT,
+                    longitude:FLOAT,
+                    pm2_5:FLOAT,
+                    pm10:FLOAT,
+                    o3:FLOAT,
+                    co:FLOAT,
+                    no2:FLOAT,
+                    temperature:FLOAT,
+                    humidity:FLOAT,
+                    wind_speed:FLOAT,
+                    wind_deg:FLOAT,
+                    pm2_5_corrected:FLOAT,
+                    pm10_corrected:FLOAT,
+                    pm2_5_predicted:FLOAT,
+                    pm10_predicted:FLOAT
+                """,
+                create_disposition=WriteToBigQuery.CreateDisposition.CREATE_IF_NEEDED,
+                write_disposition=WriteToBigQuery.WriteDisposition.WRITE_APPEND
+            )
+        )
+
+if __name__ == "__main__":
+    run()
