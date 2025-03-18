a8eb2c19321929ab3b23e7aa43baff7dd53bbee5
mYsutomo10 <57779080+mYsutomo10@users.noreply.github.com>
2025-03-19 02:11:18 +0700

    update dataflow job

--- data-processing/dataflow_job.py
+++ data-processing/dataflow_job.py
@@ -1,7 +1,8 @@
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
+from apache_beam.options.pipeline_options import PipelineOptions
 import json
 from datetime import datetime
+from google.cloud import firestore
 
 class ParsePubSubMessage(beam.DoFn):
     def process(self, message):
@@ -9,10 +10,10 @@ class ParsePubSubMessage(beam.DoFn):
         attributes = data.get("message", {}).get("attributes", {})
         
         sensor_data = {
-            "device_id": attributes.get("device_id", ""),
-            "timestamp": attributes.get("timestamp", ""),
-            "latitude": float(attributes.get("latitude", 0)),
-            "longitude": float(attributes.get("longitude", 0)),
+            "location": attributes.get("location", ""),
+            "dt": attributes.get("dt", ""),
+            "lat": float(attributes.get("lat", 0)),
+            "lon": float(attributes.get("lon", 0)),
             "pm2_5": float(attributes.get("pm2_5", 0)),
             "pm10": float(attributes.get("pm10", 0)),
             "o3": float(attributes.get("o3", 0)),
@@ -23,13 +24,11 @@ class ParsePubSubMessage(beam.DoFn):
 
 class CleanSensorData(beam.DoFn):
     def process(self, element):
-        # Format timestamp
         try:
-            element["timestamp"] = datetime.strptime(element["timestamp"], "%Y-%m-%d %H:%M:%S")
+            element["dt"] = datetime.strptime(element["dt"], "%Y-%m-%d %H:%M:%S")
         except ValueError:
-            return  # Drop invalid data
+            return  
 
-        # Handle missing & outliers (contoh: pm2.5 harus dalam range 0-500)
         if not (0 <= element["pm2_5"] <= 500):
             element["pm2_5"] = 0
         if not (0 <= element["pm10"] <= 600):
@@ -37,23 +36,28 @@ class CleanSensorData(beam.DoFn):
 
         yield element
 
-from google.cloud import firestore
-
 class JoinWeatherData(beam.DoFn):
     def setup(self):
         self.db = firestore.Client()
 
     def process(self, element):
-        lat, lon = element["latitude"], element["longitude"]
-        query = self.db.collection(f"weather_pollution_{lat}_{lon}").order_by("dt", direction=firestore.Query.DESCENDING).limit(1)
+        location_name = element["location"]
+        query = (
+            self.db.collection("weather_pollution")
+            .document(location_name)
+            .collection("readings")
+            .order_by("dt", direction=firestore.Query.DESCENDING)
+            .limit(1)
+        )
         docs = query.stream()
 
         for doc in docs:
             weather_data = doc.to_dict()
             element.update({
-                "temperature": weather_data["list"][0]["main"]["temp"],
-                "humidity": weather_data["list"][0]["main"]["humidity"],
-                "wind_speed": weather_data["list"][0]["wind"]["speed"]
+                "temperature": weather_data["main"]["temp"],
+                "humidity": weather_data["main"]["humidity"],
+                "wind_speed": weather_data["wind"]["speed"],
+                "wind_deg": weather_data["wind"]["deg"]
             })
         yield element
 
@@ -62,18 +66,21 @@ class SaveToFirestore(beam.DoFn):
         self.db = firestore.Client()
 
     def process(self, element):
-        doc_ref = self.db.collection("processed_data").document()
-        doc_ref.set(element)
+        doc_ref = (
+            self.db.collection("processed_data")
+            .document(element["location"])
+            .collection("sensor_info").add(element)
+        )
         yield element
 
 class SaveToBigQuery(beam.DoFn):
     def process(self, element):
         from apache_beam.io.gcp.bigquery import WriteToBigQuery
         yield beam.Row(
-            device_id=element["device_id"],
-            timestamp=element["timestamp"].isoformat(),
-            latitude=element["latitude"],
-            longitude=element["longitude"],
+            location=element["location"],
+            timestamp=element["dt"].isoformat(),
+            latitude=element["lat"],
+            longitude=element["lon"],
             pm2_5=element["pm2_5"],
             pm10=element["pm10"],
             o3=element["o3"],
@@ -81,7 +88,8 @@ class SaveToBigQuery(beam.DoFn):
             no2=element["no2"],
             temperature=element["temperature"],
             humidity=element["humidity"],
-            wind_speed=element["wind_speed"]
+            wind_speed=element["wind_speed"],
+            wind_deg=element["wind_deg"]
         )
 
 def run():
